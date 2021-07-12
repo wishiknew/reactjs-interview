65dbb262de39fea39b347603795f2bfcd6ba72cf
Ram Manohar <rammanohar@inncrewin.com>
2021-07-12 18:05:51 +0530

    interview question

---
+++ src/component/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+  
+    render() {
+        return ( <h2 className="text-center">Home</h2>);
+    }
+
+}
+
+export default Home;
